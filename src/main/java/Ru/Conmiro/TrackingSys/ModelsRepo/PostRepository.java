60a01f1f84d377606bc7969f5170a0e3ea7feea7
TwoMenBand <tmb.command@gmail.com>
2023-03-22 00:46:35 +0300

    feat:mysql config and create model for DB

---
+++ src/main/java/Ru/Conmiro/TrackingSys/ModelsRepo/PostRepository.java
@@ -0,0 +1,8 @@
+package Ru.Conmiro.TrackingSys.ModelsRepo;
+
+import Ru.Conmiro.TrackingSys.models.Post;
+import org.springframework.data.repository.CrudRepository;
+
+public interface PostRepository extends CrudRepository<Post, Long> {
+
+}
--- src/main/java/Ru/Conmiro/TrackingSys/controllers/SenderController.java
+++ src/main/java/Ru/Conmiro/TrackingSys/controllers/SenderController.java
@@ -1,14 +1,22 @@
 package Ru.Conmiro.TrackingSys.controllers;
 
+import Ru.Conmiro.TrackingSys.ModelsRepo.PostRepository;
+import Ru.Conmiro.TrackingSys.models.Post;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+
 @Controller
 public class SenderController {
+    @Autowired
+    private PostRepository postRepository;
     @GetMapping("/SenderPage")
     public String SenderMain(Model model){
         model.addAttribute("title", "Cтраница отправителя");
+        Iterable<Post> posts = postRepository.findAll();
+        model.addAttribute("posts", posts);
         return "sender-page";
     }
 }
